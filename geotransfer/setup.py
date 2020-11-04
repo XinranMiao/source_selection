4c6a4eca2d22a9008f0b5e36037b567147b41454
Kris Sankaran <sankaran.kris@gmail.com>
2020-11-04 11:19:14 -0600

    start setting up python package

---
+++ geotransfer/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setuptools.setup(
+    name="geotransfer",
+    version="0.0.0",
+    author="Xinran & Kris",
+    author_email="",
+    description="Package for geographic transfer learning experiments",
+    url="https://github.com/XinranMiao/Transfer_Learning_Remote_Sensing",
+    install_requires=required,
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
