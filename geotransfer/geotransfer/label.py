ec3cd3434f891b1140f241191d59ec6e20b30c5c
Kris Sankaran <sankaran.kris@gmail.com>
2020-11-04 11:57:13 -0600

    move some functions into package

--- geotransfer/geotransfer/label.py
+++ geotransfer/geotransfer/label.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import numpy as np
-
-import sen12ms_dataLoader as loader
+from .sen12ms_dataLoader import LCBands
 
 label_idx = {'Original':np.array(range(17))+1,
         'Simplified':np.array([1,1,1,1,1,2,2,3,3,4,5,6,7,6,8,9,10])}
@@ -18,29 +17,27 @@ class Labels:
             for x in it:
                 x[...] =np.array(simplify(x))
         return(a)
-    
-    def labels_prob(self): 
+
+    def labels_prob(self):
         proportion = np.zeros(10)
         for i in range(10):
             #proportion.append(np.count_nonzero(self==i+1)/(256**2))
             proportion[i] = np.count_nonzero(self==i+1)/(256**2)
         return(proportion)
-    
+
     def image_label(self):
         return(self.argmax()+1)
-    
+
     def image_label_name(self):
         return(labels[self+1])
 
 
 def get_label(filename):
-    bands = loader.LCBands.IGBP
-    bands = bands.value
+    bands = LCBands.IGBP.value
     with rasterio.open(filename) as patch:
-        data = patch.read(bands) 
+        data = patch.read(bands)
         bounds = patch.bounds
     sim = Labels.simplify_labels(data)
     prob = Labels.labels_prob(sim)
     label = Labels.image_label(prob)
     return label
-        
