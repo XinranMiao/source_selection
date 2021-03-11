6cdf017fcfae41c2db4b79389c7f2042e2345a12
XinranMiao <69163928+XinranMiao@users.noreply.github.com>
2021-03-11 11:01:06 -0600

    modified the code

--- DeepGlobe/DeepGlobe/code.py
+++ DeepGlobe/DeepGlobe/code.py
@@ -31,6 +31,7 @@ from train1 import *
 from features import *
 from unet import *
 from data_deepglobe import *
+# Set the arguments --------------------------------
 args = {
     "batch_size": 8,
     "epochs": 50,
@@ -38,6 +39,8 @@ args = {
     "device": torch.device('cuda' if torch.cuda.is_available() else 'cpu') ,# set to "cuda" if GPU is available
     "n_components":2
 }
+
+# Load data ----------------------------------------
 base_dir = "land-"
 paths = {
     "x": list(glob.glob(base_dir+"train/*sat*")),
@@ -45,6 +48,8 @@ paths = {
 }
 ds = DeepGlobeDataset(x_paths = paths["x"], y_paths = paths["y"])
 print('loaded dataset')
+
+# Split dataset into 7:1.5:1.5
 validation_split = .15
 test_split = .15
 validation_size = round(len(ds) * validation_split)
@@ -58,11 +63,17 @@ test_loader = DataLoader(test_ds, batch_size=args["batch_size"], shuffle=True)
 print(len(train_loader.dataset), len(validation_loader.dataset),len(test_loader.dataset))
 
 print('Splitted dataset')
+
+
+# Feature extraction ---------------------------------
 vggmodel = models.vgg16(pretrained=True).to(args["device"])
 resnet18 = models.resnet18(pretrained=True).to(args["device"])
 print('loaded vgg & resnet model')
+
 layer_resnet=[resnet18.conv1,resnet18.bn1,resnet18.relu,resnet18.maxpool,resnet18.layer1,resnet18.avgpool]
 layer_vgg = [vggmodel.features,vggmodel.avgpool]
+
+# Extract features from a sub-network of resnet and vgg.
 for i, d in enumerate(train_loader):
     x = d['image'].to(args["device"])
     vgg_feature=get_activations(vggmodel,layer_vgg,x,args['device'])
@@ -76,6 +87,8 @@ for i, d in enumerate(train_loader):
     print(i,'vgg',vgg_features.shape,'resnet',resnet_feature.shape)
 np.save('vgg_features.npy', np.array(vgg_features))
 np.save('resnet_features.npy', np.array(resnet_features))
+
+# Extract VGG features (have the results already on Research Drive -> not running this time)
 #start_time = time.time()
 #for i, d in enumerate(train_loader):
  #   x = d['image'].to(args["device"])
@@ -87,6 +100,8 @@ np.save('resnet_features.npy', np.array(resnet_features))
     #print('i=',i,'x_features.shape = ',x_features.shape)
     #print("--- %s seconds ---" % (time.time() - start_time))
 x_features = np.load('x_features_cropped.npy')
+
+# Dimensionality reduction & clustering -------------------
 pca_2d = PCA(n_components=args['n_components'])
 pca_2d.fit(x_features)
 pca_2d_features = pca_2d.transform(x_features)
@@ -94,10 +109,13 @@ print(pca_2d_features.shape)
 kmeans = KMeans(n_clusters=4)
 kmeans.fit(pca_2d_features)
 'kmeans done'
+
+
+# Training UNET ------------------------------------------
 model = Unet(3, 7,2).to(args["device"])
 optimizer = torch.optim.Adam(model.parameters(), lr=args["lr"])
 print('start training')
-#train_epoch(model, train_loader, optimizer, args['device'], epoch=10)
+train_epoch(model, train_loader, optimizer, args['device'], epoch=10)
 print('start validating')
 l=validate(model, test_loader,args['device'])
 print(l)
