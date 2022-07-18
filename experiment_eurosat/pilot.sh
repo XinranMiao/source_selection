26a877a39df525205fa00db57155ef7e49d32f69
xmiao27 <xmiao27@submit2.chtc.wisc.edu>
2022-07-18 10:07:29 -0500

    CHTC jobs

---
+++ experiment_eurosat/pilot.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+export PATH=$PWD/python/bin:$PATH
+export PYTHONPATH=$PWD/packages
+export HOME=$PWD
+source activate example-environment
+
+git clone https://github.com/XinranMiao/source_selection
+cd experiment_eurosat
+cp /staging/xmiao27/metadata.csv .
+
+python3 pilot_run.py
