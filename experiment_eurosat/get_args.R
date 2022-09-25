a8d4011f0b547b1ccd5cc77a560aa4084b0faba3
XinranMiao <69163928+XinranMiao@users.noreply.github.com>
2022-09-25 09:36:21 -0500

    update arguments

--- experiment_eurosat/get_args.R
+++ experiment_eurosat/get_args.R
@@ -11,6 +11,6 @@ expand.grid(country = c("France", "Latvija", "United Kingdom", "Sverige", "Slove
 # cluster the countries
 expand.grid(target_task = 0:4,
             algorithm = c("bandit", "random"),
-            target_size = c(320, 640, 1600)) %>%
+            target_size = c(160, 320, 640)) %>%
   write.table(file = "arg_clusters.txt", quote = FALSE, sep = ",",
               col.names = FALSE, row.names = FALSE)
